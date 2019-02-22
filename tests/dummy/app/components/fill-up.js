627aabe0b08a4b81ce4cd586fc338f66645d2fb4
Chad Carbert <chadcarbert@me.com>
2019-02-22 18:15:13 +0100

    Test with element returned from fillUp property and computed alias

--- tests/dummy/app/components/fill-up.js
+++ tests/dummy/app/components/fill-up.js
@@ -1,16 +1,14 @@
 import Component from '@ember/component';
 import layout from '../templates/components/fill-up';
 import { fillUp } from 'ember-fill-up';
+import { readOnly } from '@ember/object/computed';
 
 export default Component.extend({
   layout,
 
-  dimensions: fillUp(function() {
-    let element = this.get('element');
+  width: readOnly('watchedElement.offsetWidth'),
 
-    return {
-      width: element.offsetWidth,
-      height: element.offsetHeight
-    }
+  watchedElement: fillUp(function() {
+    return this.get('element');
   })
 });
