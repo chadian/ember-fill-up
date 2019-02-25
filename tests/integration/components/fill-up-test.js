382621acc784ef6ae03c1376fb3a012d680cd3e1
Chad Carbert <chadcarbert@me.com>
2019-02-25 21:26:18 +0100

    Add basic fill-up component test

--- tests/integration/components/fill-up-test.js
+++ tests/integration/components/fill-up-test.js
@@ -1,26 +1,31 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | fill-up', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{fill-up}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
     await render(hbs`
-      {{#fill-up}}
-        template block text
-      {{/fill-up}}
+    <style>
+      #container {
+        width: 500px;
+        height: 250px;
+      }
+    </style>
+
+    {{#fill-up id="container" as |size|}}
+      <div id="width">
+        width: {{size.width}}
+      </div>
+      <div id="height">
+        height: {{size.height}}
+      </div>
+    {{/fill-up}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(find('#width').textContent.trim(), 'width: 500');
+    assert.equal(find('#height').textContent.trim(), 'height: 250');
   });
 });
