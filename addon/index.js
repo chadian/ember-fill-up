9fff8c4fe4e6cb467bde6df883dbb70b228b4a71
Chad Carbert <chadcarbert@me.com>
2019-02-25 21:25:13 +0100

    Fix bug with css zoom affecting width/height reported from getBoundingClientRect

--- addon/index.js
+++ addon/index.js
@@ -3,4 +3,10 @@ import SizeProperty from './-property';
 let defaultPropertyFn = element => element;
 
 export let sizeChange = (fn) => new SizeProperty(fn || defaultPropertyFn);
-export let size = () => new SizeProperty(element => element.getBoundingClientRect());
+
+export let size = () => new SizeProperty(element => {
+  return {
+    width: element.offsetWidth,
+    height: element.offsetHeight
+  }
+});
