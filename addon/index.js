4e328da3520d7b3ff03647fbd8ffcf162684faa7
Chad Carbert <chadcarbert@me.com>
2019-03-31 23:41:50 +0200

    Opt for comparison property lookup instead of number arguments

--- addon/index.js
+++ addon/index.js
@@ -5,9 +5,9 @@ export let HEIGHT = Symbol();
 
 let defaultPropertyFn = element => element;
 
-let dimensionComparison = (compareFunction) => ((number, dimension = WIDTH) => {
-  return new SizeProperty(element => {
-    number = Number(number);
+let dimensionComparison = (compareFunction) => ((comparisonProperty, dimension = WIDTH) => {
+  return new SizeProperty(comparisonProperty, function(element) {
+    let number = Number(this.get(comparisonProperty));
 
     if (Number.isNaN(number)) {
       return false;
@@ -18,7 +18,8 @@ let dimensionComparison = (compareFunction) => ((number, dimension = WIDTH) => {
   });
 });
 
-export let sizeChange = (fn) => new SizeProperty(fn || defaultPropertyFn);
+export let sizeChange = (fn) => new SizeProperty(typeof fn === 'function' ? fn : defaultPropertyFn);
+
 export let size = () => new SizeProperty(element => {
   return {
     width: element.offsetWidth,
