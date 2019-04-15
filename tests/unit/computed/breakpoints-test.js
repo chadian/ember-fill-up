426d5052bbd2b81d18b59d92b4bc66b088745855
Chad Carbert <chadcarbert@me.com>
2019-04-15 22:49:50 +0200

    Add tests for multiple sequences of layers

--- tests/unit/computed/breakpoints-test.js
+++ tests/unit/computed/breakpoints-test.js
@@ -65,6 +65,92 @@ module('Unit | computed | breakpoints', function() {
       assert.deepEqual(result, expected);
     });
 
+    module('it can handle multiple layering sequences', function() {
+      test('it works when falling in the gap between layering sequences', function (assert) {
+        let currentBreakPoint = 675;
+
+        let definitions = [
+          from(500, 'from-500-to-550'),
+          layer(550, 'from-550-to-600'),
+          layer(600, 'from-600-to-650'),
+          to(650),
+          from(700, 'from-700-to-750'),
+          layer(750, 'from-750-to-800'),
+          layer(800, 'from-800-to-850'),
+          to(850)
+        ];
+
+        let expected = {
+          'from-500-to-550': false,
+          'from-550-to-600': false,
+          'from-600-to-650': false,
+          'from-700-to-750': false,
+          'from-750-to-800': false,
+          'from-800-to-850': false,
+        };
+
+        let result = match(currentBreakPoint, definitions);
+
+        assert.deepEqual(result, expected);
+      });
+
+      test('it works when falling in one of the layering sequences', function (assert) {
+        let currentBreakPoint = 600;
+
+        let definitions = [
+          from(500, 'from-500-to-550'),
+          layer(550, 'from-550-to-600'),
+          layer(600, 'from-600-to-650'),
+          to(650),
+          from(700, 'from-700-to-750'),
+          layer(750, 'from-750-to-800'),
+          layer(800, 'from-800-to-850'),
+          to(850)
+        ];
+
+        let expected = {
+          'from-500-to-550': false,
+          'from-550-to-600': false,
+          'from-600-to-650': true,
+          'from-700-to-750': false,
+          'from-750-to-800': false,
+          'from-800-to-850': false,
+        };
+
+        let result = match(currentBreakPoint, definitions);
+
+        assert.deepEqual(result, expected);
+      });
+
+      test('it works with truthy layers in multiple layering sequences', function (assert) {
+        let currentBreakPoint = 525;
+
+        let definitions = [
+          from(500, 'from-500-to-550'),
+          layer(550, 'from-550-to-600'),
+          layer(600, 'from-600-to-650'),
+          to(650),
+          from(525, 'from-525-to-575'),
+          layer(575, 'from-575-to-625'),
+          layer(625, 'from-625-to-675'),
+          to(675)
+        ];
+
+        let expected = {
+          'from-500-to-550': true,
+          'from-550-to-600': false,
+          'from-600-to-650': false,
+          'from-525-to-575': true,
+          'from-575-to-625': false,
+          'from-625-to-675': false,
+        };
+
+        let result = match(currentBreakPoint, definitions);
+
+        assert.deepEqual(result, expected);
+      });
+    });
+
     module('with head, body, and tail layers', function() {
       test('it works with head layers', function (assert) {
         let currentBreakPoint = 500;
