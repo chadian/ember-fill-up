109436b54c9cb91d58c51bc44f0bc2ccb69a74cb
Chad Carbert <chadcarbert@me.com>
2019-09-13 22:18:29 -0400

    Refactor fill-up component to use element modifiers

--- tests/integration/components/fill-up-test.js
+++ tests/integration/components/fill-up-test.js
@@ -8,45 +8,137 @@ import waitForSizeChange from 'dummy/tests/helpers/wait-for-size-change';
 module('Integration | Component | fill-up', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it reports width and height set in pixels', async function(assert) {
-    await render(hbs`
-      <style>
-        #container {
-          width: 500px;
-          height: 250px;
-        }
-      </style>
+  module('dimensions', function() {
+    test('it reports width and height set in pixels', async function(assert) {
+      await render(hbs`
+        <style>
+          #container {
+            width: 500px;
+            height: 250px;
+          }
+        </style>
 
-      {{#fill-up id="container" as |size|}}
-        <div id="width">
-          width: {{size.width}}
-        </div>
-        <div id="height">
-          height: {{size.height}}
-        </div>
-      {{/fill-up}}
-    `);
+        <FillUp id="container" as |size|>
+          <div id="width">
+            width: {{size.width}}
+          </div>
+          <div id="height">
+            height: {{size.height}}
+          </div>
+        </FillUp>
+      `);
 
-    assert.equal(find('#width').textContent.trim(), 'width: 500');
-    assert.equal(find('#height').textContent.trim(), 'height: 250');
+      // await this.pauseTest();
+
+      assert.equal(find('#width').textContent.trim(), 'width: 500');
+      assert.equal(find('#height').textContent.trim(), 'height: 250');
+    });
+
+    test('it reports width and height set based on percentages', async function(assert) {
+      await render(hbs`
+        <style>
+          #outer-container {
+            width: 100px;
+            height: 100px;
+          }
+
+          #container {
+            width: 20%;
+            height: 10%;
+          }
+        </style>
+
+        <div id="outer-container">
+          <FillUp id="container" as |size|>
+            <div id="width">
+              width: {{size.width}}
+            </div>
+            <div id="height">
+              height: {{size.height}}
+            </div>
+          </FillUp>
+        </div>
+      `);
+
+      assert.equal(find('#width').textContent.trim(), 'width: 20');
+      assert.equal(find('#height').textContent.trim(), 'height: 10');
+    });
+
+    test('it reports width and height set based on percentages', async function(assert) {
+      await render(hbs `
+        <style>
+          #container {
+            width: 200px;
+            border-left: 50px;
+            border-right: 30px;
+
+            height: 100px;
+            border-top: 10px;
+            border-bottom: 20px;
+
+            border-color: black;
+            border-style: solid;
+          }
+        </style>
+
+        <FillUp id="container" as |size|>
+          <div id="width">
+            width: {{size.width}}
+          </div>
+          <div id="height">
+            height: {{size.height}}
+          </div>
+        </FillUp>
+      `);
+
+      assert.equal(find('#width').textContent.trim(), 'width: 280');
+      assert.equal(find('#height').textContent.trim(), 'height: 130');
+    });
+
+    test('it detects changes in width and height', async function(assert) {
+      await render(hbs `
+        <style>
+          #container {
+            width: 100px;
+            height: 125px;
+          }
+        </style>
+
+        <FillUp id="container" as |size|>
+          <div id="width">
+            width: {{size.width}}
+          </div>
+          <div id="height">
+            height: {{size.height}}
+          </div>
+        </FillUp>
+      `);
+
+      assert.equal(find('#width').textContent.trim(), 'width: 100');
+      assert.equal(find('#height').textContent.trim(), 'height: 125');
+
+      let container = find("#container");
+      container.style.width = "300px";
+      container.style.height = "600px";
+
+      await waitForSizeChange();
+
+      assert.equal(find('#width').textContent.trim(), 'width: 300');
+      assert.equal(find('#height').textContent.trim(), 'height: 600');
+    });
   });
 
-  test('it reports width and height set based on percentages', async function(assert) {
-    await render(hbs`
-      <style>
-        #outer-container {
-          width: 100px;
-          height: 100px;
-        }
+  module('curly component invokation', function() {
+    test("it renders", async function(assert) {
+      await render(hbs`
+        <style>
+          .ember-fill-up {
+            width: 100px;
+            height: 125px;
+          }
+        </style>
 
-        #container {
-          width: 20%;
-          height: 10%;
-        }
-      </style>
-
-      <div id="outer-container">
-        {{#fill-up id="container" as |size|}}
+        {{#fill-up as |size|}}
           <div id="width">
             width: {{size.width}}
           </div>
@@ -54,73 +146,51 @@ module('Integration | Component | fill-up', function(hooks) {
             height: {{size.height}}
           </div>
         {{/fill-up}}
-      </div>
-    `);
+      `);
 
-    assert.equal(find('#width').textContent.trim(), 'width: 20');
-    assert.equal(find('#height').textContent.trim(), 'height: 10');
-  });
+      assert.equal(find("#width").textContent.trim(), "width: 100");
+      assert.equal(find("#height").textContent.trim(), "height: 125");
 
-  test('it reports width and height set based on percentages', async function(assert) {
-    await render(hbs `
-      <style>
-        #container {
-          width: 200px;
-          border-left: 50px;
-          border-right: 30px;
+      let container = find(".ember-fill-up");
+      container.style.width = "300px";
+      container.style.height = "600px";
 
-          height: 100px;
-          border-top: 10px;
-          border-bottom: 20px;
+      await waitForSizeChange();
 
-          border-color: black;
-          border-style: solid;
-        }
-      </style>
+      assert.equal(find("#width").textContent.trim(), "width: 300");
+      assert.equal(find("#height").textContent.trim(), "height: 600");
+    });
 
-      {{#fill-up id="container" as |size|}}
-        <div id="width">
-          width: {{size.width}}
-        </div>
-        <div id="height">
-          height: {{size.height}}
-        </div>
-      {{/fill-up}}
-    `);
+    test("it supports additional classes via `classNames`", async function(assert) {
+      await render(hbs`
+        <style>
+          .container {
+            width: 100px;
+            height: 125px;
+          }
+        </style>
 
-    assert.equal(find('#width').textContent.trim(), 'width: 280');
-    assert.equal(find('#height').textContent.trim(), 'height: 130');
-  });
+        {{#fill-up classNames="container" as |size|}}
+          <div id="width">
+            width: {{size.width}}
+          </div>
+          <div id="height">
+            height: {{size.height}}
+          </div>
+        {{/fill-up}}
+      `);
 
-  test('it detects changes in width and height', async function(assert) {
-    await render(hbs `
-      <style>
-        #container {
-          width: 100px;
-          height: 125px;
-        }
-      </style>
+      assert.equal(find("#width").textContent.trim(), "width: 100");
+      assert.equal(find("#height").textContent.trim(), "height: 125");
 
-      {{#fill-up id="container" as |size|}}
-        <div id="width">
-          width: {{size.width}}
-        </div>
-        <div id="height">
-          height: {{size.height}}
-        </div>
-      {{/fill-up}}
-    `);
+      let container = find(".container");
+      container.style.width = "300px";
+      container.style.height = "600px";
 
-    assert.equal(find('#width').textContent.trim(), 'width: 100');
-    assert.equal(find('#height').textContent.trim(), 'height: 125');
+      await waitForSizeChange();
 
-    let container = find("#container");
-    container.style.width = "300px";
-    container.style.height = "600px";
-
-    await waitForSizeChange();
-
-    assert.equal(find('#width').textContent.trim(), 'width: 300');
-    assert.equal(find('#height').textContent.trim(), 'height: 600');
+      assert.equal(find("#width").textContent.trim(), "width: 300");
+      assert.equal(find("#height").textContent.trim(), "height: 600");
+    });
   });
 });
