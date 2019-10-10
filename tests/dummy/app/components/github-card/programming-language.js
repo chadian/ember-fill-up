ec89ee351b39bd43c41136cc5f2014fdfa82f412
Chad Carbert <chadcarbert@me.com>
2019-10-09 22:40:05 -0400

    Add github-card/programming-language component

--- tests/dummy/app/components/github-card/programming-language.js
+++ tests/dummy/app/components/github-card/programming-language.js
@@ -1,6 +1,33 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import layout from '../../templates/components/github-card/programming-language';
 
 export default Component.extend({
-  layout
+  layout,
+
+  tagName: '',
+
+  language: '',
+
+  languageColor: computed('language', function() {
+    const languageColorMap = {
+      javascript: 'yellow',
+      elixir: 'purple',
+      ruby: 'red',
+      rust: 'darkred',
+      go: 'teal'
+    };
+
+    return languageColorMap[this.language] || 'silver';
+  }),
+
+  languageLabel: computed('language', function() {
+    let language = this.language.toUpperCase();
+
+    if (language === "JAVASCRIPT") {
+      return "JS";
+    }
+
+    return language;
+  })
 });
