d30f857b06a992db44853a09045645d7415c7a27
Chad Carbert <chadcarbert@me.com>
2019-10-09 22:39:31 -0400

    Add github-card/stars component

--- tests/dummy/app/components/github-card/stars.js
+++ tests/dummy/app/components/github-card/stars.js
@@ -1,6 +1,21 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
 import layout from '../../templates/components/github-card/stars';
 
 export default Component.extend({
-  layout
+  layout,
+
+  tagName: '',
+
+  stars: 0,
+
+  formattedStars: computed('stars', function() {
+    const stars = this.stars;
+
+    if (stars > 1000) {
+      return Math.round((stars / 1000) * 100) / 100 + "k";
+    }
+
+    return stars;
+  })
 });
