8aa74319ef459ffed88ac705b9affd7bc07d70f7
Chad Carbert <chadcarbert@me.com>
2019-10-15 21:00:41 +0200

    Add dimensions to template helpers

--- addon/helpers/fill-up-eq.js
+++ addon/helpers/fill-up-eq.js
@@ -1,8 +1,8 @@
 import { helper } from '@ember/component/helper';
 import { eq } from 'ember-fill-up/definitions';
 
-export function fillUpEq([value]) {
-  return eq(value);
+export function fillUpEq([value], { dimension } = {}) {
+  return eq(value, { dimension });
 }
 
 export default helper(fillUpEq);
--- addon/helpers/fill-up-gt.js
+++ addon/helpers/fill-up-gt.js
@@ -1,8 +1,8 @@
 import { helper } from '@ember/component/helper';
 import { gt } from 'ember-fill-up/definitions';
 
-export function fillUpGt([value]) {
-  return gt(value);
+export function fillUpGt([value], { dimension } = {}) {
+  return gt(value, { dimension });
 }
 
 export default helper(fillUpGt);
