65f2f6e1a69be6e60b96c35f3d503085f1335573
Chad Carbert <chadcarbert@me.com>
2019-12-16 12:15:47 -0500

    Add polyfill usage instructions (#27)

---
+++ tests/dummy/app/initializers/setup-resize-detector-polyfill.js
@@ -0,0 +1,16 @@
+import ResizeObserver from 'resize-observer-polyfill';
+
+export function initialize() {
+  if (!window.ResizeObserver) {
+    // eslint-disable-next-line no-console
+    console.info(
+      'initializer:setup-resize-detector-polyfill: ResizeObserver not found. Polyfilling...'
+    );
+    window.ResizeObserver = ResizeObserver;
+  }
+}
+
+export default {
+  before: 'register-resize-observer-detector',
+  initialize
+};
